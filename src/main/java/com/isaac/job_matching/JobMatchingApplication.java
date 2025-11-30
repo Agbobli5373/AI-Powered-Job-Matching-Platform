9e9a89250b4456b56a787aea186757e00b14a6a2
Agbobli Isaac <agbobliisaackwadzo@gmail.com>
2025-11-30 04:11:22 +0000

    Initial commit from Specify template

---
+++ src/main/java/com/isaac/job_matching/JobMatchingApplication.java
@@ -0,0 +1,13 @@
+package com.isaac.job_matching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobMatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobMatchingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isaac/job_matching/JobMatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaac.job_matching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobMatchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
